4968c4162c9824464349ecb97ddd75c0abec344c
Steve Henderson <hevets@gmail.com>
2016-05-06 21:13:10 -0700

    adding some more buttons

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // label
-        var label = UILabel()
+        self.label = UILabel()
         label.frame = CGRectMake(150, 150, 60, 60)
         label.text = "0"
-        self.view.addSubview(label)
+        self.view.addSubview(self.label)
         
         // button
         var button = UIButton()
@@ -25,6 +28,25 @@ class ViewController: UIViewController {
         button.setTitle("click", forState: .Normal)
         button.setTitleColor(UIColor.blueColor(), forState: .Normal)
         self.view.addSubview(button)
+        
+        
+        var bgcolorbutton = UIButton()
+        bgcolorbutton.frame = CGRectMake(150, 350, 60, 60)
+        bgcolorbutton.setTitle("click", forState: .Normal)
+        bgcolorbutton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(bgcolorbutton)
+        
+        bgcolorbutton.addTarget(self, action: "changeBgColor", forControlEvents: .TouchUpInside)
+        button.addTarget(self, action: "incrementCount", forControlEvents: .TouchUpInside)
+    }
+    
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
+    }
+    
+    func changeBgColor() {
+        self.view.backgroundColor = UIColor.redColor()
     }
 
 }
