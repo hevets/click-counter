7d73d2200036c05c4cb68b8d561ac267fa2c204b
Steve Henderson <hevets@gmail.com>
2016-05-08 12:04:28 -0700

    using storyboard to wire up events

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -11,42 +11,16 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label:UILabel!
+    @IBOutlet var label:UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // label
-        self.label = UILabel()
-        label.frame = CGRectMake(150, 150, 60, 60)
-        label.text = "0"
-        self.view.addSubview(self.label)
-        
-        // button
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(button)
-        
-        
-        var bgcolorbutton = UIButton()
-        bgcolorbutton.frame = CGRectMake(150, 350, 60, 60)
-        bgcolorbutton.setTitle("click", forState: .Normal)
-        bgcolorbutton.setTitleColor(UIColor.blueColor(), forState: .Normal)
-        self.view.addSubview(bgcolorbutton)
-        
-        bgcolorbutton.addTarget(self, action: "changeBgColor", forControlEvents: .TouchUpInside)
-        button.addTarget(self, action: "incrementCount", forControlEvents: .TouchUpInside)
     }
     
-    func incrementCount() {
-        self.count++
+    @IBAction func incrementCount() {
+        self.count += 1
         self.label.text = "\(self.count)"
-    }
-    
-    func changeBgColor() {
-        self.view.backgroundColor = UIColor.redColor()
     }
 
 }
